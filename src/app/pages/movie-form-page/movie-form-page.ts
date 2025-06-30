77d152d966dcbda39e49f5928c5002a55f8f9095
Harold Cortes <harcor59p@gmail.com>
2025-06-30 07:46:34 -0500

    manejo de rutas

--- src/app/pages/movie-form-page/movie-form-page.ts
+++ src/app/pages/movie-form-page/movie-form-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
+import { MovieFormComponent } from '../../components/movie-form-component/movie-form-component';
 
 @Component({
   selector: 'app-movie-form-page',
-  imports: [Header ],
+  imports: [Header , MovieFormComponent],
   templateUrl: './movie-form-page.html',
   styleUrl: './movie-form-page.css'
 })
